a91d7d848e072eb41b4fa4af754cb2b648f0ea9c
zhanglihua.river <zhanglihua.river@bytedance.com>
2024-12-19 21:16:08 +0800

    feat: 704 & 27

---
+++ array/[1] 704.go
@@ -0,0 +1,43 @@
+package array
+
+// 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否
+// 则返回 -1。
+//
+// 示例 1:
+//
+// 输入: nums = [-1,0,3,5,9,12], target = 9
+// 输出: 4
+// 解释: 9 出现在 nums 中并且下标为 4
+//
+// 示例 2:
+//
+// 输入: nums = [-1,0,3,5,9,12], target = 2
+// 输出: -1
+// 解释: 2 不存在 nums 中因此返回 -1
+//
+// 提示：
+//
+// 你可以假设 nums 中的所有元素是不重复的。
+// n 将在 [1, 10000]之间。
+// nums 的每个元素都将在 [-9999, 9999]之间。
+func search(nums []int, target int) int {
+	low, high := 0, len(nums)-1
+	for low <= high {
+		mid := (low + high) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		if target < nums[mid] {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+	return -1
+}
+
+/**
+- 前提条件：无重复元素+有序
+- 区间：左闭右闭
+- 结束条件：low == high的时候，指向的元素要不要加入判断？——要！所以当low > high时跳出循环
+*/
