e7d0a4f97cf7f2dfb2d318b643c5db7b60ecfd03
zhanglihua.river <zhanglihua.river@bytedance.com>
2024-12-28 13:16:29 +0800

    feat: 303

---
+++ array/[12] 303-区域和检索.go
@@ -0,0 +1,57 @@
+package array
+
+// NumArray 给定一个整数数组 nums，处理以下类型的多个查询:
+//
+// 计算索引 left 和 right （包含 left 和 right）之间的 nums 元素的 和 ，其中 left <= right
+//
+// 实现 NumArray 类：
+//
+// NumArray(int[] nums) 使用数组 nums 初始化对象
+// int sumRange(int i, int j) 返回数组 nums 中索引 left 和 right 之间的元素的 总和 ，包含 left 和
+// right 两点（也就是 nums[left] + nums[left + 1] + ... + nums[right] )
+//
+// 示例 1：
+//
+// 输入：
+// ["NumArray", "sumRange", "sumRange", "sumRange"]
+// [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+// 输出：
+// [null, 1, -1, -3]
+//
+// 解释：
+// NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+// numArray.sumRange(0, 2); // return 1 ((-2) + 0 + 3)
+// numArray.sumRange(2, 5); // return -1 (3 + (-5) + 2 + (-1))
+// numArray.sumRange(0, 5); // return -3 ((-2) + 0 + 3 + (-5) + 2 + (-1))
+//
+// 提示：
+//
+// 1 <= nums.length <= 10⁴
+// -10⁵ <= nums[i] <= 10⁵
+// 0 <= i <= j < nums.length
+// 最多调用 10⁴ 次 sumRange 方法
+type NumArray struct {
+	preSum []int
+}
+
+func Constructor(nums []int) NumArray {
+	preSum := make([]int, len(nums))
+	preSum[0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		preSum[i] += preSum[i-1] + nums[i]
+	}
+	return NumArray{preSum}
+}
+
+func (this *NumArray) SumRange(left int, right int) int {
+	if left == 0 {
+		return this.preSum[right]
+	}
+	return this.preSum[right] - this.preSum[left-1]
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * obj := Constructor(nums);
+ * param_1 := obj.SumRange(left,right);
+ */
