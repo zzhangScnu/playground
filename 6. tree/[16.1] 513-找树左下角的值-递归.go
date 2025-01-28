80eff47d28fdcef20cecde9c9b2564ff6779c306
zhanglihua.river <zhanglihua.river@bytedance.com>
2025-01-28 21:50:50 +0800

    feat: 513

--- 6. tree/[16.1] 513-找树左下角的值-递归.go
+++ 6. tree/[16.1] 513-找树左下角的值-递归.go
@@ -19,7 +19,33 @@ package tree
 // 二叉树的节点个数的范围是 [1,10⁴]
 //
 // -2³¹ <= Node.val <= 2³¹ - 1
+
+var maximumDepth int
+
+var bottomLeftValue int
+
 func findBottomLeftValue(root *TreeNode) int {
-	// todo：也需要用到回溯！
-	return -1
+	maximumDepth = 0
+	bottomLeftValue = 0
+	doFindBottomLeftValue(root, 1)
+	return bottomLeftValue
 }
+
+func doFindBottomLeftValue(node *TreeNode, depth int) {
+	if depth > maximumDepth && node.Left == nil && node.Right == nil {
+		maximumDepth = depth
+		bottomLeftValue = node.Val
+		return
+	}
+	if node.Left != nil {
+		doFindBottomLeftValue(node.Left, depth+1)
+	}
+	if node.Right != nil {
+		doFindBottomLeftValue(node.Right, depth+1)
+	}
+}
+
+/**
+在遍历到深度更深的叶子节点时，更新结果值。
+由于使用了左优先的遍历方式，会在更新深度时，仅更新一次结果值。从而保证取的是左叶子节点的值。
+*/
