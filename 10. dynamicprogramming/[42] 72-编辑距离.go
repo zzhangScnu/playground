d0e769316448965bf2c5f56ba1fad6a175d6dc5a
zhanglihua.river <zhanglihua.river@bytedance.com>
2025-03-17 17:25:40 +0800

    feat: 739

--- 10. dynamicprogramming/[42] 72-编辑距离.go
+++ 10. dynamicprogramming/[42] 72-编辑距离.go
@@ -46,7 +46,7 @@ func minDistance72(word1 string, word2 string) int {
 	}
 	for i := 1; i <= l1; i++ {
 		for j := 1; j <= l2; j++ {
-			if word1[i-1] == word2[i-1] {
+			if word1[i-1] == word2[j-1] {
 				dp[i][j] = dp[i-1][j-1]
 			} else {
 				dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
