5d86ddd7dfd31a2dab189bf8c5282b449ac95b32
zhanglihua.river <zhanglihua.river@bytedance.com>
2025-03-27 17:45:26 +0800

    feat: 题解

--- 1. array/[41] 922-按奇偶排序数组II.go
+++ 1. array/[41] 922-按奇偶排序数组II.go
@@ -41,8 +41,30 @@ func sortArrayByParityII(nums []int) []int {
 	return nums
 }
 
-// 每次只处理一个分支的好处；同时处理带来的混乱：
-
 /**
-The user's current code has a loop where they check even and odd separately, then swap if both are wrong. But the issue is that after incrementing even or odd in the first two if statements, the third condition might not trigger when only one is wrong. For example, if even is correct (even index has even number), so even increments, but odd is incorrect. Then, the third condition might not be met because even is already moved. This could leave some elements unswapped.
+思路：
+双指针，odd指向奇数索引位置，even指向偶数索引位置，共同向前推进，每次走2步。
+每轮只处理一种情况：
+1. 若nums[odd]为奇数，合法 -> 推进odd，进入下一轮循环；
+2. 若nums[even]为偶数，合法 -> 推进even，进入下一轮循环；
+最终一定会找到一对不合法的odd & even组合，此时：
+3. 若nums[odd]为偶数，nums[even]为奇数，不合法 -> 交换nums[odd]和nums[even]，进入下一轮循环。
+
+
+之前的处理是写了个if & else if & else，会导致遗漏元素。
+for even, odd := 0, 1; even < n && odd < n; {
+	if nums[even]%2 == 0 {
+		even += 2
+	}
+	if nums[odd]%2 == 1 {
+		odd += 2
+	}
+	if nums[even]%2 == 1 && nums[odd]%2 == 0 {
+		nums[even], nums[odd] = nums[odd], nums[even]
+		even += 2
+		odd += 2
+	}
+}
+问题是，每轮循环可能处理多个场景，导致混乱。
+只有在even和odd都不合法时，才会进入交换逻辑。只有一个不合法的场景会被遗漏。
 */
