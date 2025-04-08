ca504a93059e895724e03c34c1da4211cc873ef6
zhanglihua.river <zhanglihua.river@bytedance.com>
2025-04-08 11:43:30 +0800

    feat: 题解

--- 10. dynamicprogramming/[46] 132-分割回文串II.go
+++ 10. dynamicprogramming/[46] 132-分割回文串II.go
@@ -73,6 +73,9 @@ if s[j + 1 ... i] == 回文子串
 此时切割次数：前者 = dp[j]，后者 = 1
 因每次都需要判断一个子串是否回文，需要引入备忘录。
 
+同时，在遍历过程中，如果发现[0 ... i]本身就是回文串，则最小分割次数为1，
+保持默认值即可，也无需遍历j来试图找到最小切割次数。
+
 
 初始化：
 对于长度为k的字符串，最大的切割次数为k，即每个字符作为一个回文子串。
@@ -81,4 +84,47 @@ if s[j + 1 ... i] == 回文子串
 
 遍历方向：
 从左到右。
+*/
+
+/**
+如何判断回文串并初始化备忘录：
+
+
+一开始的做法时间复杂度O(n^3)：
+
+isPalindrome := func(s string, start, end int) bool {
+	for i, j := beginIdx, endIdx; i < j; i, j = i+1, j-1 {
+		if str[i] != str[j] {
+			return false
+		}
+	}
+	return true
+}
+
+for i := 0; i < n; i++ {
+	for j := 0; j < n; j++ {
+		memo[i][j] = isPalindrome(s, i, j)
+	}
+}
+
+但有一种简单做法，是通过状态转移递推而来，时间复杂度为O(n^2)：
+memo := make([][]bool, n)
+for i := 0; i < n; i++ {
+	memo[i] = make([]bool, n)
+	memo[i][i] = true // 单个字符本身构成回文串
+	if i > 0 && s[i-1] == s[i] { // 长度为2且字符相同的字符串构成回文串
+		memo[i-1][i] = true
+	}
+}
+// 这里先固定字符串长度length
+// 再对字符串的每一个起点i，计算出终点j = i + length - 1
+// 判断s[i ... j]是否回文串
+// 为什么要这么做？因为长度为length的字符串是否回文子串，依赖于长度为length-2的字符串是否回文子串递推而来
+// 即长度较小 -> 长度较大，所以需要循环递增length，每次固定length，处理完所有可能的字符串，再对length++，继续处理下一批可能的字符串
+for length := 3; length <= n; length++ { // 因为长度为1和2的子字符串已初始化，这里从长度为3开始
+	for i := 0; i <= n-length; i++ { // 因为i和j都需要在s的长度范围内，而j = i + length - 1 < n，即i < n - length + 1
+		j := i + length - 1
+		memo[i][j] = memo[i+1][j-1] && s[i] == s[j] // s[i ... j]是否回文串，可以拆解为子问题：s[i + 1 ... j - 1]是否回文串 && s[i]和s[j]是否相等
+	}
+}
 */
--- 10. dynamicprogramming/[47] 673-最长递增子序列的个数.go
+++ 10. dynamicprogramming/[47] 673-最长递增子序列的个数.go
@@ -33,15 +33,15 @@ func findNumberOfLIS(nums []int) int {
 			if nums[j] >= nums[i] {
 				continue
 			}
-			if length[j] == length[i] {
+			if length[j]+1 == length[i] {
 				count[i] += count[j]
 			}
-			if length[j]+1 == length[i] {
+			if length[j]+1 > length[i] {
 				count[i] = count[j]
 				length[i] = length[j] + 1
-				maxLen = max(maxLen, length[i])
 			}
 		}
+		maxLen = max(maxLen, length[i])
 	}
 	var res int
 	for i := 0; i < n; i++ {
@@ -58,22 +58,35 @@ DP数组及下标含义：
 - count[i]：nums[0 ... i]间最长递增子序列的个数。
 - len[i]：nums[0 ... i]间最长递增子序列的长度。
 
+
 递推公式：
 对于case [1, 3, 5, 4, 7]，
 for i -> [0, n)
 for j -> [0, i)
 固定i作为右边界，对每一个小于i的j进行判断：
 若 nums[j] < nums[i]，则表示nums[j ... i]单调递增，
-	- 若len[j] == len[i]，则表示找到了另一组长度相同的递增子序列。
-	  形如[1, 3, 5] 和[1, 3, 4]。此时j = 2，i = 3，len[2] == len[3] == 3，count[2] = 1，
-	  故count[i] += count[j]，len[i] = len[j]
-	- 若len[j] + 1 == len[i]，则表示找到了另一组更长的递增子序列。
-	  形如[1, 3, 5] 和[1, 3, 5, 7]。此时j = 2，i = 4，len[2] == 3，len[4] == 4，count[2] = 1，
- 	  故count[i] = count[j]，len[i] = len[j] + 1
+ 	- 若len[j] + 1 > len[i]，则表示找到了另一组更长的递增子序列。
+
+	  对于i == 4，nums[i] == 7，
+	  j == 2，nums[j] == 5时，
+	  找到[1, 3, 5, 7]，此时len[2] + 1 > len[4]。因为len[2]在i == 2时递推得到结果3，而len[4]此时仍为初始值1。
+ 	  故count[i] = count[j]，len[i] = len[j] + 1，即len[4] = len[2] + 1 = 4，count[4] = count[2] = 1。
+
+	- 若len[j] + 1 == len[i]，则表示找到了另一组长度相同的递增子序列。
+
+	  外循环仍固定在i == 4，nums[i] == 7，
+	  内循环向前推进到达j == 3，nums[j] == 5时，
+	  找到[1, 3, 4, 7]，此时len[4] = len[3] + 1。因为之前已经找到了[1, 3, 5, 7]，本次找到的递增子序列长度相同。
+	  故count[i] += count[j]，len[i] = len[j]，即len[4]不变，len[4] = len[3] = 4，count[4] = count[4] + count[3] = 1 + 1 = 2。
+
+更新maxLen的位置应该是在处理完每个右边界i之后。无论是否需要在[0 ... i]之间找j作为分割点，
+都应该动态更新最大长度。
+
 
 初始化：
 count中每个单元格的值均为1，表示自身组成一个递增子序列。
 len中每个单元格的值均为1，表示自身组成递增子序列的长度为1。
+
 
 遍历方向：
 先遍历i，再遍历j。固定i的情况下，对[0 ... i]中的每一个子序列[0 ... j]，判断和nums[i]之间是否构成递增关系。
