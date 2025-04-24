810493951e594aa85258f35c02f6f12a317cba2b
zhanglihua.river <zhanglihua.river@bytedance.com>
2025-04-24 11:49:05 +0800

    feat: 234

--- linklist/[15] 234-回文链表.go
+++ linklist/[15] 234-回文链表.go
@@ -46,13 +46,56 @@ func reverse(head *ListNode) *ListNode {
 	return pre
 }
 
+var left *ListNode
+
+func isPalindromeRecursively(head *ListNode) bool {
+	left = head
+	return doIsPalindromeRecursively(head)
+}
+
+func doIsPalindromeRecursively(right *ListNode) bool {
+	if right == nil {
+		return true
+	}
+	isEqual := doIsPalindromeRecursively(right.Next)
+	isEqual = isEqual && (left.Val == right.Val)
+	left = left.Next
+	return isEqual
+}
+
 /**
-暴力解法：
+暴力解法1：
 遍历链表 -> 转换为数组 -> 双指针判断是否回文数组
 时间复杂度：O(n)
 空间复杂度：O(n)
 
-原地解法：
+
+暴力解法2：
+反转链表 -> 比较正反链表是否相等
+时间复杂度：O(n)
+空间复杂度：O(n)
+
+
+递归解法：isPalindromeRecursively
+类似二叉树的遍历，可以将链表看作无分叉的、每个节点只有一个子节点的树，不同的遍历顺序代表不同的输出顺序。
+如：
+func traverse(node *ListNode) {
+	if node == nil {
+		return node
+	}
+	traverse(node.Next)
+	fmt.Println(node.Val)
+}
+后序遍历是在递归下钻，直至叶子节点的下一层后触底反弹，在反弹过程中输出节点值。
+核心逻辑是将链表节点依次放入栈，再依次弹出，所以输出顺序是逆序的。
+同理，如果是先序遍历，则为正序，跟迭代遍历链表效果一致。
+那么可以利用后序遍历特性，在递归的逆序访问中，将当前节点right，与正序访问的节点left，进行比较，如果不一致，则不是回文链表。
+注意，递归结果需要变量接收，且与当前层的比较结果做与运算。本质上是left和right从两边向中间夹逼，直至left遍历到链表终点，right遍历到链表起点。
+时间复杂度：O(n)
+空间复杂度：O(n)，需要额外堆栈支持。
+
+
+原地解法：isPalindrome
 遍历链表 -> 找到中间节点 -> 反转后半段链表 -> 从头开始对比两段链表是否相等
 时间复杂度：O(n)
 空间复杂度：O(1)
