a364dbf066625aaf4766fa6be3e84ff8d1ede79d
zhanglihua.river <zhanglihua.river@bytedance.com>
2025-05-09 14:22:02 +0800

    feat: 题解

--- greedy/[22] 32-最长有效括号-贪心.go
+++ greedy/[22] 32-最长有效括号-贪心.go
@@ -1,4 +1,4 @@
-package dynamicprogramming
+package greedy
 
 // 给你一个只包含 '(' 和 ')' 的字符串，找出最长有效（格式正确且连续）括号子串的长度。
 //
@@ -56,26 +56,18 @@ func longestValidParentheses(s string) int {
 	return longest
 }
 
-func longestValidParenthesesII(s string) int {
-	var longest int
-	n := len(s)
-	dp := make([]int, n)
-	for i := 1; i < n; i++ {
-		if s[i] == '(' {
-			continue
-		}
-		if s[i-1] == '(' {
-			dp[i] = 2
-			if i >= 2 {
-				dp[i] += dp[i-2]
-			}
-		} else if s[i-1] == ')' && i >= dp[i-1]+1 && s[i-dp[i-1]-1] == '(' {
-			dp[i] = dp[i-1] + 2
-			if i >= dp[i-1]+2 {
-				dp[i] += dp[i-dp[i-1]-2]
-			}
-		}
-		longest = max(longest, dp[i])
-	}
-	return longest
-}
+/**
+思路：
+从左到右遍历字符串，计算左括号和右括号的数量left和right。
+
+如果left == right，表示遍历到目前为止，左右括号都是匹配的合法串，此时更新最大长度；
+
+否则如果left < right，由于合法括号串要求顺序性和连续性，即先左后右。
+如果左括号数量已经不足了，后续即使在右括号之后再遇到新的左括号，也无法弥补缺失，组成新的一对括号。
+此时重置left和right，重新从下一个字符开始计算。
+
+这样遍历下来，会漏掉一种情况：即左括号的数量一直比右括号多，但实际上是有合法括号对的，如 (()。
+
+所以需要我们从右到左再次遍历，重新计算，唯一不同的地方是重置的判断条件变为right < left。
+因为逆向遍历时，合法的括号是先右再左的。
+*/
