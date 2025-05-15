241a606dec72bf1eadf4a8d8fb3a28b97c5439d5
zhanglihua.river <zhanglihua.river@bytedance.com>
2025-05-15 14:33:35 +0800

    feat: 703

---
+++ heap/[10] 703-数据流中的第K大元素.go
@@ -0,0 +1,74 @@
+package heap
+
+import "container/heap"
+
+// KthLargest 设计一个找到数据流中第 k 大元素的类（class）。注意是排序后的第 k 大元素，不是第 k 个不同的元素。
+//
+// 请实现 KthLargest 类：
+//
+// KthLargest(int k, int[] nums) 使用整数 k 和整数流 nums 初始化对象。
+// int add(int val) 将 val 插入数据流 nums 后，返回当前数据流中第 k 大的元素。
+//
+// 示例 1：
+//
+// 输入： ["KthLargest", "add", "add", "add", "add", "add"] [[3, [4, 5, 8, 2]], [3]
+// , [5], [10], [9], [4]]
+//
+// 输出：[null, 4, 5, 5, 8, 8]
+//
+// 解释：
+//
+// KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]); kthLargest.add(3); /
+// / 返回 4 kthLargest.add(5); // 返回 5 kthLargest.add(10); // 返回 5 kthLargest.add(9);
+// // 返回 8 kthLargest.add(4); // 返回 8
+//
+// 示例 2：
+//
+// 输入： ["KthLargest", "add", "add", "add", "add"] [[4, [7, 7, 7, 7, 8, 3]], [2],
+// [10], [9], [9]]
+//
+// 输出：[null, 7, 7, 7, 8]
+//
+// 解释： KthLargest kthLargest = new KthLargest(4, [7, 7, 7, 7, 8, 3]);
+// kthLargest.add(2); // 返回 7
+// kthLargest.add(10); // 返回 7
+// kthLargest.add(9); // 返回 7
+// kthLargest.add(9); // 返回 8
+//
+// 提示：
+//
+// 0 <= nums.length <= 10⁴
+// 1 <= k <= nums.length + 1
+// -10⁴ <= nums[i] <= 10⁴
+// -10⁴ <= val <= 10⁴
+// 最多调用 add 方法 10⁴ 次
+type KthLargest struct {
+	Capacity int
+	*MaxHeap
+}
+
+func KthLargestConstructor(k int, nums []int) *KthLargest {
+	maxHeap := &KthLargest{Capacity: k}
+	heap.Init(maxHeap)
+	for _, num := range nums {
+		heap.Push(maxHeap, num)
+		if maxHeap.Len() > k {
+			heap.Pop(maxHeap)
+		}
+	}
+	return maxHeap
+}
+
+func (this *KthLargest) Add(val int) int {
+	heap.Push(this.MaxHeap, val)
+	if this.MaxHeap.Len() > this.Capacity {
+		heap.Pop(this.MaxHeap)
+	}
+	return (*this.MaxHeap)[0]
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * obj := Constructor(k, nums);
+ * param_1 := obj.Add(val);
+ */
