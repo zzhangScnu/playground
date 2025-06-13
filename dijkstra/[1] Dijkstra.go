004141b4a8e73c52988ccdc7733b67790ef89ac2
zhanglihua.river <zhanglihua.river@bytedance.com>
2025-06-13 11:22:40 +0800

    feat: 题解

--- dijkstra/[1] Dijkstra.go
+++ dijkstra/[1] Dijkstra.go
@@ -6,8 +6,8 @@ import (
 )
 
 type Node struct {
-	node     int
-	distance int
+	node     int // 当前节点
+	distance int // 从起点到当前节点的最短路径的权重和
 }
 
 type MinHeap []*Node
@@ -37,9 +37,9 @@ func (h *MinHeap) Pop() any {
 }
 
 type Dijkstra struct {
-	adjacent [][]int
-	distance []int
-	start    int
+	adjacent [][]int // 邻接表，存储图信息
+	distance []int   // 结果集，distance[i]表示从start到节点i的最短路径的权重和
+	start    int     // 起始节点
 }
 
 func NewDijkstra(n int, adjacent [][]int, start int) *Dijkstra {
@@ -55,23 +55,36 @@ func NewDijkstra(n int, adjacent [][]int, start int) *Dijkstra {
 	}
 }
 
+/*
+根据实际情况，节点间的权重计算方式可能不同
+*/
 func (d *Dijkstra) weight(x, y int) int {
 	return x - y
 }
 
 func (d *Dijkstra) calculateDistance() {
+	// 优先级队列，队口元素为当前【从start开始的权重和最小的最短路径】到达的节点，体现贪心思想
 	minHeap := &MinHeap{}
 	heap.Init(minHeap)
+	// 将起点推入优先级队列。节点到自身的距离为0
 	minHeap.Push(&Node{
 		node:     d.start,
 		distance: 0,
 	})
 	for minHeap.Len() > 0 {
 		cur := minHeap.Pop().(*Node)
+		// 若结果集中[start, cur]的权重和已经比队列中[start, cur]的权重和更小，即可能这条路径已经被其他节点更新优化过了，当前路径不再适用。
+		// 即与cur相邻的节点to，其最短路径[start, to]，不会因为当前队列取出的子路径[start, cur]更新为更优权重和
 		if cur.distance > d.distance[cur.node] {
 			continue
 		}
+		// 获取cur的相邻节点
 		for _, to := range d.adjacent[cur.node] {
+			// 尝试更新to的结果，即找到[start, to]的一条更优最短路径
+			// 根据cur计算出的[start, to]的最短路径的权重和 weight = [start, cur]的权重和 + [cur, to]的权重和
+			// 即 weight = 结果集中start到cur的最短路径权重和 + 动态计算的cur到to的距离
+			// 如果 重新计算的weight 比 记录在案的distance[to] 更优，则更新distance，
+			// 并将to和weight作为候选入列，在之后用于更新to的相邻节点的路径权重和。
 			if d.distance[cur.node]+d.weight(cur.node, to) > d.distance[to] {
 				continue
 			}
--- unionfindset/[1] 并查集实现.go
+++ unionfindset/[1] 并查集实现.go
@@ -30,6 +30,22 @@ func (u *UnionFindSet) IsConnected(x, y int) bool {
 	return rootX == rootY
 }
 
+/*
+通过递归方式在查找节点x时，将[根节点 -> x节点]路径上的所有节点，
+都挂在根节点下。
+使得树的高度维持在常数，操作的时间复杂度降至O(1)。
+
+parent[x]表示x节点指向的父节点，初始化时parent[x] = x，即每个节点自身是独立的联通分量。此时联通分量数量 == 节点数量。
+
+if x == parent[x]，即x的父节点为自己，
+则表示x仍是初始化时的独立联通分量，直接返回parent[x]。
+
+if x != parent[x]，即x的父节点是其他节点，表示x已经挂载在其他联通分量下了，
+then 递归向上寻找，直到 x == parent[x]，即到达根节点。
+此时触底反弹，层层返回这个根节点，在每一层用parent[x]接收，即将每层节点直接指向这个根节点。
+
+注意，第一次写的时候将if写成for，揉杂了递归和迭代。
+*/
 func (u *UnionFindSet) find(x int) int {
 	if x != u.parent[x] {
 		u.parent[x] = u.find(u.parent[x])
@@ -37,6 +53,20 @@ func (u *UnionFindSet) find(x int) int {
 	return u.parent[x]
 }
 
+/*
+通过迭代方式，实现跟递归效果相同的路径压缩。
+
+但过程稍有不同：
+
+if x == parent[x]，即x的父节点为自己，
+则表示x仍是初始化时的独立联通分量，直接返回parent[x]。
+
+if x != parent[x]，即x的父节点是其他节点，表示x已经挂载在其他联通分量下了，
+then 令x的父节点 = x父节点的父节点 -> 此时将x的层级压缩，向上提了一层。
+循环此操作，给x赋值[x节点 -> 根节点]路径上的每层节点，将每个x都向上提一层。
+直到 x == parent[x]，即到达根节点。
+最终返回根节点x。
+*/
 func (u *UnionFindSet) findIteratively(x int) int {
 	for x != u.parent[x] {
 		u.parent[x] = u.parent[u.parent[x]]
