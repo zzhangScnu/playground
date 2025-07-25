9635265ee7473522fa151f868355d0558af1f044
zhanglihua.river <zhanglihua.river@bytedance.com>
2025-07-25 21:48:09 +0800

    feat: 391

--- array/[51] 391-完美矩形.go
+++ array/[51] 391-完美矩形.go
@@ -5,13 +5,13 @@ import "math"
 // rectangles，⾥⾯装着若⼲四元组
 // (x1,y1,x2,y2)，每个四元组就是记录⼀个矩形的左下⻆和右上⻆坐标。
 func isRectangleCover(rectangles [][]int) bool {
-	X1, Y1, X2, Y2 := math.MaxInt, math.MinInt, math.MinInt, math.MaxInt
+	X1, Y1, X2, Y2 := math.MaxInt, math.MaxInt, math.MinInt, math.MinInt
 	actualArea := 0
 	points := make(map[[2]int]interface{})
 	for _, rectangle := range rectangles {
 		x1, y1, x2, y2 := rectangle[0], rectangle[1], rectangle[2], rectangle[2]
-		X1, Y1 = min(X1, x1), max(Y1, y1)
-		X2, Y2 = max(X2, x2), min(Y2, y2)
+		X1, Y1 = min(X1, x1), min(Y1, y1)
+		X2, Y2 = max(X2, x2), max(Y2, y2)
 		actualArea += (x2 - x1) * (y2 - y1)
 		p1, p2, p3, p4 := [2]int{x1, y2}, [2]int{x2, y2}, [2]int{x1, y1}, [2]int{x2, y1}
 		for _, point := range [][2]int{p1, p2, p3, p4} {
