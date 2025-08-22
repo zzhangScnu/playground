2357882c40560868d9f5460c005d3e4d08614ea7
zhanglihua.river <zhanglihua.river@bytedance.com>
2025-08-22 15:50:43 +0800

    feat: 221-迭代

---
+++ dynamicprogramming/[61.2] 221-最大正方形-递归.go
@@ -0,0 +1,76 @@
+package dynamicprogramming
+
+// 在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。
+//
+// 示例 1：
+//
+// 输入：matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"]
+// ,["1","0","0","1","0"]]
+// 输出：4
+//
+// 示例 2：
+//
+// 输入：matrix = [["0","1"],["1","0"]]
+// 输出：1
+//
+// 示例 3：
+//
+// 输入：matrix = [["0"]]
+// 输出：0
+//
+// 提示：
+//
+// m == matrix.length
+// n == matrix[i].length
+// 1 <= m, n <= 300
+// matrix[i][j] 为 '0' 或 '1'
+func maximalSquareRecursively(matrix [][]byte) int {
+	m, n := len(matrix), len(matrix[0])
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	var traverse func(i, j int) int
+	traverse = func(i, j int) int {
+		if i >= m || j >= n {
+			return 0
+		}
+		if matrix[i][j] == '0' {
+			return 0
+		}
+		dp[i][j] = 1 + min(traverse(i, j+1), traverse(i+1, j), traverse(i+1, j+1))
+		return dp[i][j]
+	}
+	var res int
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			res = max(res, dp[i][j])
+		}
+	}
+	return res * res
+}
+
+/**
+DP数组及下标含义
+- i & j：二维矩阵坐标(i, j)
+- DP[i][j]：(i, j)作为左上顶点时，最大正方形的边长
+
+
+递推公式
+right = dp[i][j + 1]
+down = dp[i + 1][j]
+diagonal = dp[i + 1][j + 1]
+
+dp[i][j] = matrix[i][j] + min(right, down, diagonal)
+
+
+遍历方向
+由递推公式可知，i & j 依赖于 i + 1 & j + 1推导而来，故从下到上，由右到左。
+
+
+初始化
+需提前处理base case。将dp初始化为 (m + 1) * (n + 1)的大小，
+dp[m][0 ... n]：表示行越界的情况，初始化为0
+dp[0...m][n]：表示列越界的情况，初始化为0
+结果存储在dp[0][0]中
+*/
