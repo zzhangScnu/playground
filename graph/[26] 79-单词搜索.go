f6ab15359e1cb78bd60c66da2aece71c0e8f4772
zhanglihua.river <zhanglihua.river@bytedance.com>
2025-09-10 20:55:34 +0800

    feat: 79

--- graph/[26] 79-单词搜索.go
+++ graph/[26] 79-单词搜索.go
@@ -1,5 +1,7 @@
 package graph
 
+import "slices"
+
 // 给定一个 m x n 二维字符网格 board 和一个字符串单词 word 。如果 word 存在于网格中，返回 true ；否则，返回 false 。
 //
 // 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
@@ -33,16 +35,22 @@ package graph
 // 进阶：你可以使用搜索剪枝的技术来优化解决方案，使其在 board 更大的情况下可以更快解决问题？
 func exist(board [][]byte, word string) bool {
 	m, n := len(board), len(board[0])
-	movements := [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
-	var initialVisited func() [][]bool
-	initialVisited = func() [][]bool {
-		visited := make([][]bool, m)
-		for i := 0; i < m; i++ {
-			visited[i] = make([]bool, n)
+	countb, countw := make(map[byte]int), make(map[byte]int)
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			countb[board[i][j]]++
 		}
-		return visited
 	}
-	visited := initialVisited()
+	for i := 0; i < len(word); i++ {
+		countw[word[i]]++
+		if countw[word[i]] > countb[word[i]] {
+			return false
+		}
+	}
+	if countb[word[0]] > countb[word[len(word)-1]] {
+		slices.Reverse([]byte(word))
+	}
+	movements := [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
 	var traverse func(x, y, targetIndex int) bool
 	traverse = func(x, y, targetIndex int) bool {
 		if targetIndex == len(word) {
@@ -51,19 +59,19 @@ func exist(board [][]byte, word string) bool {
 		if x < 0 || x >= m || y < 0 || y >= n {
 			return false
 		}
-		if visited[x][y] {
+		if board[x][y] == ' ' {
 			return false
 		}
 		if board[x][y] != word[targetIndex] {
 			return false
 		}
-		visited[x][y] = true
+		board[x][y] = ' '
 		for _, movement := range movements {
 			if traverse(x+movement[0], y+movement[1], targetIndex+1) {
 				return true
 			}
 		}
-		visited[x][y] = false
+		board[x][y] = word[targetIndex]
 		return false
 	}
 	for i := 0; i < m; i++ {
