9304befe6702462d6c85f4b74bcb509be70f0e7a
zhanglihua.river <zhanglihua.river@bytedance.com>
2025-09-25 08:18:27 +0800

    feat: 201

---
+++ bitwise/[5] 201-数字范围按位与.go
@@ -0,0 +1,30 @@
+package bitwise
+
+// 给你两个整数 left 和 right ，表示区间 [left, right] ，返回此区间内所有数字 按位与 的结果（包含 left 、right 端点）
+// 。
+//
+// 示例 1：
+// / 输入：left = 5, right = 7
+// 输出：4
+//
+// 示例 2：
+//
+// 输入：left = 0, right = 0
+// 输出：0
+//
+// 示例 3：
+//
+// 输入：left = 1, right = 2147483647
+// 输出：0
+//
+// 提示：
+//
+// 0 <= left <= right <= 2³¹ - 1
+func rangeBitwiseAnd(left int, right int) int {
+	var i int
+	for left != right {
+		left, right = left>>1, right>>1
+		i++
+	}
+	return left << i
+}
