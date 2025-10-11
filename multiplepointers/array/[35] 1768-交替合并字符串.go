220c421277b4c2078ac4a832900acb09a3a4a900
zhanglihua.river <zhanglihua.river@bytedance.com>
2025-10-11 17:13:50 +0800

    feat: 1768

---
+++ multiplepointers/array/[35] 1768-交替合并字符串.go
@@ -0,0 +1,68 @@
+package array
+
+// 给你两个字符串 word1 和 word2 。请你从 word1 开始，通过交替添加字母来合并字符串。如果一个字符串比另一个字符串长，就将多出来的字母追加到
+// 合并后字符串的末尾。
+//
+// 返回 合并后的字符串 。
+//
+// 示例 1：
+//
+// 输入：word1 = "abc", word2 = "pqr"
+// 输出："apbqcr"
+// 解释：字符串合并情况如下所示：
+// word1：  a   b   c
+// word2：    p   q   r
+// 合并后：  a p b q c r
+//
+// 示例 2：
+//
+// 输入：word1 = "ab", word2 = "pqrs"
+// 输出："apbqrs"
+// 解释：注意，word2 比 word1 长，"rs" 需要追加到合并后字符串的末尾。
+// word1：  a   b
+// word2：    p   q   r   s
+// 合并后：  a p b q   r   s
+//
+// 示例 3：
+//
+// 输入：word1 = "abcd", word2 = "pq"
+// 输出："apbqcd"
+// 解释：注意，word1 比 word2 长，"cd" 需要追加到合并后字符串的末尾。
+// word1：  a   b   c   d
+// word2：    p   q
+// 合并后：  a p b q c   d
+//
+// 提示：
+//
+// 1 <= word1.length, word2.length <= 100
+// word1 和 word2 由小写英文字母组成
+func mergeAlternately(word1 string, word2 string) string {
+	n1, n2 := len(word1), len(word2)
+	p1, p2 := 0, 0
+	var res []byte
+	for p1 < n1 && p2 < n2 {
+		res = append(res, word1[p1], word2[p2])
+		p1++
+		p2++
+	}
+	res = append(res, word1[p1:]...)
+	res = append(res, word2[p2:]...)
+	return string(res)
+}
+
+func mergeAlternatelyII(word1 string, word2 string) string {
+	n1, n2 := len(word1), len(word2)
+	p1, p2 := 0, 0
+	res := make([]byte, 0, n1+n2)
+	for p1 < n1 || p2 < n2 {
+		if p1 < n1 {
+			res = append(res, word1[p1])
+			p1++
+		}
+		if p2 < n2 {
+			res = append(res, word2[p2])
+			p2++
+		}
+	}
+	return string(res)
+}
