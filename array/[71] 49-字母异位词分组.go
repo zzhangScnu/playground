130ce36c906474ddf7dd0f8a605ea705b358d56c
zhanglihua.river <zhanglihua.river@bytedance.com>
2025-12-27 09:08:47 +0800

    feat: 49

---
+++ array/[71] 49-字母异位词分组.go
@@ -0,0 +1,57 @@
+package array
+
+import (
+	"sort"
+)
+
+// 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+//
+// 示例 1:
+//
+// 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+//
+// 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+//
+// 解释：
+//
+// 在 strs 中没有字符串可以通过重新排列来形成 "bat"。
+// 字符串 "nat" 和 "tan" 是字母异位词，因为它们可以重新排列以形成彼此。
+// 字符串 "ate" ，"eat" 和 "tea" 是字母异位词，因为它们可以重新排列以形成彼此。
+//
+// 示例 2:
+//
+// 输入: strs = [""]
+//
+// 输出: [[""]]
+//
+// 示例 3:
+//
+// 输入: strs = ["a"]
+//
+// 输出: [["a"]]
+//
+// 提示：
+//
+// 1 <= strs.length <= 10⁴
+// 0 <= strs[i].length <= 100
+// strs[i] 仅包含小写字母
+func groupAnagrams(strs []string) [][]string {
+	sortedMap := make(map[string][]string)
+	for _, str := range strs {
+		sortedStr := sortStr(str)
+		sortedMap[sortedStr] = append(sortedMap[sortedStr], str)
+	}
+	res := make([][]string, 0, len(sortedMap))
+	for _, groupedStrs := range sortedMap {
+		res = append(res, groupedStrs)
+	}
+	return res
+}
+
+func sortStr(str string) string {
+	arr := []byte(str)
+	sort.Slice(arr, func(i, j int) bool {
+		return arr[i] < arr[j]
+	})
+	return string(arr)
+}
