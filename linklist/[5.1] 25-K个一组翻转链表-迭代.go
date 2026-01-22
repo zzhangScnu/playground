1891509d18038f22784e3786cdd21c882230eaa7
zhanglihua.river <zhanglihua.river@bytedance.com>
2026-01-22 08:08:07 +0800

    feat: 题解

--- linklist/[5.1] 25-K个一组翻转链表-迭代.go
+++ linklist/[5.1] 25-K个一组翻转链表-迭代.go
@@ -27,10 +27,8 @@ func reverseKGroup(head *ListNode, k int) *ListNode {
 	dummyHead := &ListNode{Next: head}
 	begin, from, to := dummyHead, dummyHead.Next, dummyHead.Next
 	for {
-		for i := 0; i < k-1; i++ {
-			if to != nil {
-				to = to.Next
-			}
+		for i := 0; i < k-1 && to != nil; i++ {
+			to = to.Next
 		}
 		if to == nil {
 			break
@@ -46,9 +44,9 @@ func reverseKGroup(head *ListNode, k int) *ListNode {
 
 // 前闭后闭
 func reverseBetweenNode(head *ListNode, tail *ListNode) (*ListNode, *ListNode) {
-	var pre, cur *ListNode = nil, head
+	var pre, cur, next *ListNode = nil, head, nil
 	for pre != tail {
-		next := cur.Next
+		next = cur.Next
 		cur.Next = pre
 		pre = cur
 		cur = next
@@ -63,4 +61,11 @@ func reverseBetweenNode(head *ListNode, tail *ListNode) (*ListNode, *ListNode) {
 - 该区间的前驱和后驱，为了将反转后的子链表跟原链表连接起来。
 2. to走k-1步，因为from占了一个位置，to再数k-1，才能凑齐k个节点；
 3. 需要在每次反转前，提前把next存下来，防止操作后导致的断链。
+
+注意：
+- 一般迭代做法里的区间反转入参都是左闭右闭，返回反转后的头节点和尾节点，主要有两个作用：
+	- 方便在原链表中进行连接；
+	- 需要将下一个反转区间定位出来，如begin, from, to = reversedTail, next, next。
+- to 的作用就是用于解决不足 k 个节点时需要保持原本顺序不变的问题，用 to 来标识需反转链表的尾部。
+  所以循环的条件是 i < k-1 && to != nil，即当 to 为 nil 时，说明剩余链表长度已不足 k。
 */
